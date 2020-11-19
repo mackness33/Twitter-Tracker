ffd588a39640df666246b5f8997a1107f2389a31
David Mack <mack.david.88@gmail.com>
2020-11-19 19:47:53 +0100

    :sparkles: Introducing TwitterService
    Service to manage everything regarding requests to twitter

--- service/Twitter/useful_class.py
+++ service/Twitter/useful_class.py
@@ -1,68 +1,54 @@
-from flask_login import UserMixin
-from werkzeug.security import generate_password_hash, check_password_hash
-# from service.twitter_keys import ACCESS_TOKEN
-
 import requests
 import os
 import json
 
 class Twitter():
+    # token: bearer token for the authentication
     def __init__(self, token):
         self._bearer = token
 
+    # get tweets based on the argument
     def get_tweets(self, args):
-        # bearer_token = ACCESS_TOKEN
-        url = self.create_url(args)
-        headers = self.create_headers()
-        json_response = self.connect_to_endpoint(url, headers)
+        url = self.create_url(args)                             # set up the url
+        headers = self.create_headers()                         # set up the headers
+        json_response = self.connect_to_endpoint(url, headers)  # set up the response as a json
         return json_response
 
 
-    # key = "name_of_the_field"
-    # value = list(values)
     def create_url(self, args):
         # Specify the usernames that you want to lookup below
         # You can enter up to 100 comma-separated values.
-        field = list()
-        print (type(args))
-
-        for key in args:
-            field_string = key + "="
-            print(field_string)
-            if type(args[key]) is list:
-                for val in args[key]:
-                    field_string += val
-            else:
-                field_string += args[key]
-            # print(key)
-            field.append(field_string)
-        # usernames = "usernames=TwitterDev,TwitterAPI"
-        # user_fields = "user.fields=description,created_at"
         # User fields are adjustable, options include:
         # created_at, description, entities, id, location, name,
         # pinned_tweet_id, profile_image_url, protected,
         # public_metrics, url, username, verified, and withheld
-        s = "field: "
-        for i in range(len(field)):
-            print("field: " + field[i])
-            s += field[i]
-        # print(s)
+
         url = "https://api.twitter.com/2/users/by?"
+
+        # key MUST be a parameter name
+        # value is the value of the field
         for key in args:
-            url += "{" + str(key) + "}&"
-        url = url[:-1]
-        print ("before: " + url)
-        url.format(*field)
-        print ("after: " + url)
+            url += key + "="                        # set the query key in the url
+            if type(args[key]) is list:             # if the argument of the element is a list of value
+                for val in args[key]:
+                    url += '"' + val + '",'         # add multiple values
+                url = url[:-1]                      # delete last character
+            else:
+                url += args[key]
+            url += "&"                              # add next one
+
+        url = url[:-1]                      # delete last character
 
         return url
 
-
+    # create headers
     def create_headers(self):
+        # adding the authentication token for twitter (OATH2)
         headers = {"Authorization": "Bearer {}".format(self._bearer)}
         return headers
 
 
+    # create the response
     def connect_to_endpoint(self, url, headers):
         response = requests.request("GET", url, headers=headers)
         print(response.status_code)
