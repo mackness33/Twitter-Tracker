b1c8f452905c1498ada5dc704724c8f7b4be9d3b
David Mack <mack.david.88@gmail.com>
2020-12-11 19:09:59 +0100

    :zap: Implemented and tested tweets_lookup

--- service/Twitter/useful_class.py
+++ service/Twitter/useful_class.py
@@ -2,20 +2,23 @@ import requests
 import os
 import json
 
+# TODO: if incorrect type of input raise an exception.
+# TODO: if in multiple args one doesn't exist retry starting from the next one.
+
 class Twitter():
     # token: bearer token for the authentication
     def __init__(self, token):
         self._bearer = token
+        self._base_url = "https://api.twitter.com/2/"
 
-    # get tweets based on the argument
-    def get_tweets(self, args):
-        url = self.create_url(args)                             # set up the url
-        headers = self.create_headers()                         # set up the headers
-        json_response = self.connect_to_endpoint(url, headers)  # set up the response as a json
+    #---------TWEETS LOOKUP----------
+    def tweets_lookup(self, id, **fields):
+        url = self._create_url('tweets', 'ids', id)                          # set up the url
+        headers = self._create_headers()                         # set up the headers
+        json_response = self._request_resources(url, headers)  # set up the response as a json
         return json_response
 
-
-    def create_url(self, args):
+    def _create_url(self, type, field, values):
         # Specify the usernames that you want to lookup below
         # You can enter up to 100 comma-separated values.
         # User fields are adjustable, options include:
@@ -23,7 +26,33 @@ class Twitter():
         # pinned_tweet_id, profile_image_url, protected,
         # public_metrics, url, username, verified, and withheld
 
-        url = "https://api.twitter.com/2/users/by?"
+        # add the type of request
+        url = self._base_url + type
+
+        # multiple arguments or single arg
+        if not isinstance(values, list) or len(values) == 1:
+            url += '/' + str(values)
+        else:
+            # key MUST be a parameter name
+            # value is the value of the field
+            url += '?' + field + '='
+            for value in values:                # for each value
+                print ('value: ', value)
+                url += str(value) + ','              # add value
+
+            url = url[:-1]                      # delete last character
+
+        print('url: ', url)
+        return url
+
+    def _add_fields(self, url, **fields):
+        # Specify the usernames that you want to lookup below
+        # You can enter up to 100 comma-separated values.
+        # User fields are adjustable, options include:
+        # created_at, description, entities, id, location, name,
+        # pinned_tweet_id, profile_image_url, protected,
+        # public_metrics, url, username, verified, and withheld
+        url = self._base_url + url
 
         # key MUST be a parameter name
         # value is the value of the field
@@ -42,14 +71,14 @@ class Twitter():
         return url
 
     # create headers
-    def create_headers(self):
+    def _create_headers(self):
         # adding the authentication token for twitter (OATH2)
         headers = {"Authorization": "Bearer {}".format(self._bearer)}
         return headers
 
 
     # create the response
-    def connect_to_endpoint(self, url, headers):
+    def _request_resources(self, url, headers):
         response = requests.request("GET", url, headers=headers)
         print(response.status_code)
         if response.status_code != 200:
--- web/blueprints/auth/views.py
+++ web/blueprints/auth/views.py
@@ -60,7 +60,9 @@ def base_post():
     if say[0] == "#":
         return Twitter.cursor(Twitter.search, q=say2)
     elif say[0] == '@':
-        return T.get_tweets({"usernames": say2, "user.fields": "description,created_at"})
+        return T.tweets_lookup(id=1275828087666679809)
+    elif say[0] == 'd':
+        return T.tweets_lookup(id=[20,1276230436478386177,1276501058211262464])
     elif say[0] == '$':
         return "not implemented"
 
@@ -69,105 +71,3 @@ def base_post():
 @tracker.route('/base')
 def base():
     return  render_template('SWE_interfaccia.html')
-
-
-# @auth.route('/login', methods=['POST'])
-# def login_post():
-#     form = LoginForm(request.form)
-#     print('{} tried to log in'.format(form.email.data))
-#
-#     # check if form is valid
-#     if(form.validate_on_submit()):
-#         # validating the data of the form
-#         id = form.email.data
-#
-#         if id is not None:
-#             print('id = {}'.format(id))
-#             date_end =  datetime.timedelta(seconds=360000000)
-#
-#             # create an access_token token that exists for 5 min
-#             # access_token = create_access_token(identity=id, expires_delta=date_end)
-#             # # create an access_token token
-#             # refresh_token = create_refresh_token(identity=id)
-#
-#             # adding the token to the response
-#             # TODO: try a more elegant or useful way to do it
-#             # @after_this_request
-#             # def adding(response):
-#             #     set_access_cookies(response, access_token)
-#             #     set_refresh_cookies(response, refresh_token)
-#             #     return response
-#
-#             print('Loggin {}'.format(form.email.data))
-#             return redirect(url_for('tweet.base'))
-#
-#             print('{} has been refiused'.format(form.email.data))
-#         return redirect(url_for('auth.login'))
-#     else:
-#         # TODO: handle it       -> it needs logs
-#         errors = form.errors
-#
-#     print('errors: {}'.format(errors))
-#     print('{} has been refused'.format(form.email.data))
-#     return jsonify(errors)
-#     # print('No good for my dude {}'.format(form.email.data))
-#     # return render_template('login.html', form=form)
-
-
-#---------------SIGNUP---------------
-@tracker.route('/signup')
-def signup():
-    return render_template('signup.html')
-
-
-# @auth.route('/signup', methods=['POST'])
-# def signup_post():
-#     form = SignupForm(request.form)
-#     print('{} tried to signup'.format(form.email.data))
-#
-#     # setting next
-#     # TODO: setting next. Need to redirect to the right function that return that uri
-#     # if request.args.get('next'):
-#     #     next = render_template(request.args.get('next'), form=ErrorForm())
-#     # next = url_for('auth.signup')
-#
-#     if(form.validate_on_submit()):
-#         # pwd has be added to the try so that if some errors are catched the hash won't be rememberd/recorded
-#         # if users.count(EMAIL=form.email.data) == 0:
-#             # try:
-#                 # pwd = generate_password_hash(form.password.data.strip(), method='sha512')
-#                 # users.insert_one(
-#                 #     EMAIL=form.email.data, PASSWORD=pwd, TYPE=form.type.data, NAME=form.name.data, SURNAME=form.surname.data, BIRTHDAY=form.birthday.data,
-#                 #     GENDER=form.gender.data, FISCAL_CODE=form.fiscal_code.data, COMPANY_NAME=form.company_name.data, ENTERPRISE=form.enterprise.data,
-#                 #     VAT_NUM=form.vat.data, COUNTRY=form.country.data,PROVINCE=form.province.data, ZIPCODE=form.zipcode.data, ADDRESS=form.address.data,
-#                 #     SIGNUP= signupUser(TOKEN_SIGNUP = 'Nothing for now', CONFIRMED=False), ACTIVE= False, PROJECTS = project(PROJECT=["DIMOSTRATIVO2"]),
-#                 #     SECOND_KEY = form.email.data+"_is_secret =)"
-#                 # )
-#
-#         print('{} successfully sign up'.format(form.email.data))
-#         return url_for('auth.login'), 200
-#         #     except Exception as e:
-#         #         print(e)
-#         #         print('sorry for {}, got problem with the database'.format(form.email.data))
-#         #         errors = {'name': form.email.data, 'value': 'Invalid email address'}
-#         # else:
-#         #     print('{} already exists'.format(form.email.data))
-#         #     errors = {'name': form.email.data, 'value': 'Email already exists'}
-#     else:
-#         errors = form.errors
-#
-#     print('errors: {}'.format(errors))
-#     print('No good for my dude {}'.format(form.email.data))
-#     return jsonify(errors)
-
-#---------------LOGOUT---------------
-@tracker.route('/logout')
-# @jwt_required
-def logout():
-    #TODO: add index
-    # @after_this_request
-    # def adding(response):
-    #     unset_jwt_cookies(response)
-    #     return response
-
-    return redirect(url_for('tracker.login'))
